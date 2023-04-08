a81d1453dc51473cd8b1dce1ee98f37c04e91534
francis.69 <fcastillosanabria@gmail.com>
2023-04-08 18:50:21 -0500

    actializacion responsive index

---
+++ script.js
@@ -0,0 +1,6 @@
+/* ===== Loader =====*/
+window.addEventListener('load', () => {
+    const contenedorLoader = document.querySelector('.container--loader');
+    contenedorLoader.style.opacity = 0;
+    contenedorLoader.style.visibility = 'hidden';
+})
